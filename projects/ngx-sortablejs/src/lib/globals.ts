7d61dd1338fdc166a7de2022104242ba796d9a22
Raquel <raquel.diaz@bluetab.net>
2020-08-27 12:01:28 +0200

    delete all files

--- projects/ngx-sortablejs/src/lib/globals.ts
+++
@@ -1,4 +0,0 @@
-import { InjectionToken } from '@angular/core';
-import { SortablejsOptions } from './sortablejs-options';
-
-export const GLOBALS: InjectionToken<SortablejsOptions> = new InjectionToken('Global config for sortablejs');
